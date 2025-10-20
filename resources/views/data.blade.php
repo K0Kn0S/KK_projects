b267072a4c93ed064dd724ea5b821ec3c10d49c6
K0Kn0S <kokinslauris140@gmail.com>
2025-10-20 11:32:25 +0000

    xcv

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -9,7 +9,7 @@
 
 <a href="/data/allData" class="btn btn-success">Apskatīt vissus datus</a>
 <a href="/data/allDarbinieks" class="btn btn-success">Darbinieki</a>
-<a href="/data/allAmats" class="btn btn-success">Amati</a>
+<a href="/data/allAmats" class="btn btn-success">Amatidasdawd</a>
 
     
 @endsection
