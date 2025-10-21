b112babe1fb46ade6c43f60c23015c02eaa42c8b
K0Kn0S <kokinslauris140@gmail.com>
2025-10-21 08:31:26 +0000

    mn

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -13,26 +13,25 @@
 </ul>
 </div>
 @endif
-
-<form action="/contacti/newsubmit" method="post">
+<form action="/data/all/{{$data->id}}" method="post">
     @csrf
-<h1>Kontakti</h1>
-<div class="container" style="max-width: 40%;">
+<h1>Edit</h1>
+<div class="container" style="max-width: 60%;">
     <div class="mb-3">
         <label for="name" class="form-label"></label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="vārds">
+        <input type="text" class="form-control" id="name" name="name" placeholder="Vārds">
     </div>
     <div class="mb-3">
         <label for="email" class="form-label"></label>
         <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
     </div>
-
+   
     <div class="mb-3">
         <label for="message" class="form-label"></label>
-        <textarea class="form-control" id="message" rows="4" name="message" placeholder="Message">
+        <textarea class="form-control" id="message" name="message" rows="3"></textarea>
     </div>
-
-    <button type="submit" class="btn btn-info">Submit</button>
+    <button type="submit" class="btn btn-info">Pievienot</button>
+    <button type="button" class="btn btn-danger" onclick="window.history.back()">Atcelt</button>
 </div>
 </form>
 @endsection
