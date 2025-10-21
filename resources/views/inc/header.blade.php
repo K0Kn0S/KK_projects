7e26f8d81b6efacc02b4f20d19d3719c7bf55e19
K0Kn0S <kokinslauris140@gmail.com>
2025-10-21 07:45:09 +0000

    ass

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -7,4 +7,6 @@
              <li class="nav-item"><a href="/data" class="nav-link">Dati</a>
             </li> <li class="nav-item"><a href="/contacts" class="nav-link">Contacts</a>
         </li> <li class="nav-item"><a href="#" class="nav-link">nekas</a></li>
+         <li class="nav-item"><a href="/Sign_up" class="nav-link">Reģistrēties</a></li>
+          <li class="nav-item"><a href="Login" class="nav-link">Pieslēgties</a></li>
      </ul> </header> </div>
